b028e83f3a4c3eca770140a10208bee0b958ee8a
Christian Lalinde <33659395+LalindeCyber@users.noreply.github.com>
2022-06-09 14:46:14 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,6 +2,9 @@ node default {
   file {'/root/README':
     ensure  => file,
     content => 'This is a readme',
-    owner   => 'sephihakubi',
+    owner   => 'root',
+  }
+  file {'/root/README':
+    owner   => 'root',
   }
 }
