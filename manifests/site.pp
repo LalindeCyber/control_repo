fcc6c665f9d6581df56e1f6524f170be0c4cebe3
Christian Lalinde <33659395+LalindeCyber@users.noreply.github.com>
2022-06-11 16:43:58 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,3 +3,11 @@ node default {
 node 'pandoraserveru22.pandora.local' {
   include role::master_server
 }
+
+node 'PANDORAClientU22.pandora.local' {
+  include role::app_server
+}
+
+node 'PANDORAClientU20.pandora.local' {
+  include role::db_server
+}
